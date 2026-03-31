c669176cfc7d55ad9e766862d3d9d5668d3337f0
momoleking55 <momoleking55@gmail.com>
2026-03-31 15:54:01 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://qizoeseonjwctisepmgy.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_27FHOqAxh-TgDrgmma07bg_Ahwz1QnZ'
+
+const { createClient } = supabase
+const db = createClient(SUPABASE_URL, SUPABASE_KEY)
